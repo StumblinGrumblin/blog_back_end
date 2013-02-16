1f19eb9dde3040860749b00ece28df4321376c4f
stumblingrumblin <stumblingrumblin@gmail.com>
2013-02-16 00:30:39 -0800

    Rot13 complete

--- blog/main.py
+++ blog/main.py
@@ -1,6 +1,11 @@
 import webapp2
 
+import cgi
+
 from scripts.rot13 import rot13
+
+def escape_html(s):
+  return cgi.escape(s, quote = True)
 
 form1 = '''
 <html>
@@ -9,7 +14,12 @@ form1 = '''
   </head>
   <body>
     <h1>STUMBLINGRUMBLIN.COM <br> HOMEPAGE</h1>
+    <a href="/rot13">Caesar Cipher Project</a>
+    <br>
+    <a href="/signup">Signup Page</a>
+    <br>
     <form action="http:/ww.google.com/search">
+    <h3>Search for something on Google</h3>
       <input name="q">
       <input type="submit">
     </form>
@@ -25,7 +35,7 @@ form2 = '''
   <body>
     <h2>Enter some text to Caesar Cipher</h2>
     <form method="post">
-      <textarea name="text" style="height: 100px; width: 400px;">%(cipher)s</textarea>
+      <textarea name="text" style="height: 100px; width: 400px;">%(output)s</textarea>
       <br>
       <input type="submit">
     </form>
@@ -33,24 +43,96 @@ form2 = '''
 </html>
 '''
 
+form3 = '''
+<!DOCTYPE html>
+<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+    <title>Sign Up</title>
+    <style type="text/css">
+      .label {text-align: right}
+      .error {color: red}
+    </style>
+
+  </head>
+
+  <body>
+    <h2>Signup</h2>
+    <form method="post">
+      <table>
+        <tbody><tr>
+          <td class="label">
+            Username
+          </td>
+          <td>
+            <input type="text" name="username" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Password
+          </td>
+          <td>
+            <input type="password" name="password" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Verify Password
+          </td>
+          <td>
+            <input type="password" name="verify" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+
+        <tr>
+          <td class="label">
+            Email (optional)
+          </td>
+          <td>
+            <input type="text" name="email" value="">
+          </td>
+          <td class="error">
+            
+          </td>
+        </tr>
+      </tbody></table>
+
+      <input type="submit">
+    </form>
+
+</body></html>
+'''
+
 class MainPage(webapp2.RequestHandler):
   def get(self):
     self.response.out.write(form1)
 
 class Rot13(webapp2.RequestHandler):
-  def write_form(self, cipher=""):
-    self.response.out.write(form2 %{"cipher": cipher})
+  def write_form(self, output=""):
+    self.response.out.write(form2 %{"output": escape_html(output)})
 
   def get(self):
-      self.write_form()
+    self.write_form()
 
   def post(self):
-    self.write_form("Bob Dole")
+    sin = self.request.get("text")
+    ciph = rot13(sin)
+    self.write_form(ciph)
 
-###ROT13 cipher###
-#x = "Hello!"
+class Signup(webapp2.RequestHandler):
+  def get(self):
+    self.response.out.write(form3)
 
-#print rot13(x)
 
 app = webapp2.WSGIApplication([('/', MainPage),
-  ('/rot13', Rot13)], debug=True)
+  ('/rot13', Rot13), ('/signup', Signup)], debug=True)
--- blog/scripts/rot13.py
+++ blog/scripts/rot13.py
@@ -1,4 +1,4 @@
-x = '''Hello!!! Loki ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus convallis lorem at lobortis?? Quisque tempus congue tincidunt!'''
+#x = '''Hello!!! Loki ipsum dolor sit amet, consectetur adipiscing elit. Sed cursus convallis lorem at lobortis?? Quisque tempus congue tincidunt!'''
 
 def rot13(x):
 	letter = ('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z')
