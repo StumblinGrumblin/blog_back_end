287b33feb3c35d49177cffe32a0cae9ff8cb6f70
stumblingrumblin <stumblingrumblin@gmail.com>
2013-02-17 14:35:21 -0800

    Finished HW2 Pages

--- blog/main.py
+++ blog/main.py
@@ -1,138 +1,90 @@
-import webapp2
+import os
+import re
+from string import letters
 
-import cgi
+import webapp2
+import jinja2
+
+from google.appengine.ext import db
 
 from scripts.rot13 import rot13
 
-def escape_html(s):
-  return cgi.escape(s, quote = True)
+from scripts.validate import *
 
-form1 = '''
-<html>
-  <head>
-    <title>StumblinGrumblin.com</title>
-  </head>
-  <body>
-    <h1>STUMBLINGRUMBLIN.COM <br> HOMEPAGE</h1>
-    <a href="/rot13">Caesar Cipher Project</a>
-    <br>
-    <a href="/signup">Signup Page</a>
-    <br>
-    <form action="http:/ww.google.com/search">
-    <h3>Search for something on Google</h3>
-      <input name="q">
-      <input type="submit">
-    </form>
-  </body>
-</html>
-'''
+template_dir = os.path.join(os.path.dirname(__file__), 'templates')
+jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
+                               autoescape = True)
 
-form2 = '''
-<html>
-  <head>
-    <title>Caesar Cipher</title>
-  </head>
-  <body>
-    <h2>Enter some text to Caesar Cipher</h2>
-    <form method="post">
-      <textarea name="text" style="height: 100px; width: 400px;">%(output)s</textarea>
-      <br>
-      <input type="submit">
-    </form>
-  </body>
-</html>
-'''
+def render_str(template, **params):
+    t = jinja_env.get_template(template)
+    return t.render(params)
 
-form3 = '''
-<!DOCTYPE html>
-<html><head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-    <title>Sign Up</title>
-    <style type="text/css">
-      .label {text-align: right}
-      .error {color: red}
-    </style>
+class BaseHandler(webapp2.RequestHandler):
+    def render(self, template, **kw):
+        self.response.out.write(render_str(template, **kw))
 
-  </head>
+    def write(self, *a, **kw):
+        self.response.out.write(*a, **kw)
 
-  <body>
-    <h2>Signup</h2>
-    <form method="post">
-      <table>
-        <tbody><tr>
-          <td class="label">
-            Username
-          </td>
-          <td>
-            <input type="text" name="username" value="">
-          </td>
-          <td class="error">
-            
-          </td>
-        </tr>
-
-        <tr>
-          <td class="label">
-            Password
-          </td>
-          <td>
-            <input type="password" name="password" value="">
-          </td>
-          <td class="error">
-            
-          </td>
-        </tr>
-
-        <tr>
-          <td class="label">
-            Verify Password
-          </td>
-          <td>
-            <input type="password" name="verify" value="">
-          </td>
-          <td class="error">
-            
-          </td>
-        </tr>
-
-        <tr>
-          <td class="label">
-            Email (optional)
-          </td>
-          <td>
-            <input type="text" name="email" value="">
-          </td>
-          <td class="error">
-            
-          </td>
-        </tr>
-      </tbody></table>
-
-      <input type="submit">
-    </form>
-
-</body></html>
-'''
-
-class MainPage(webapp2.RequestHandler):
+class MainPage(BaseHandler):
   def get(self):
-    self.response.out.write(form1)
+    self.render('index.html')
 
-class Rot13(webapp2.RequestHandler):
-  def write_form(self, output=""):
-    self.response.out.write(form2 %{"output": escape_html(output)})
-
+class Rot13(BaseHandler):
+  
   def get(self):
-    self.write_form()
+    self.render('rot13.html')
 
   def post(self):
-    sin = self.request.get("text")
-    ciph = rot13(sin)
-    self.write_form(ciph)
+    rot13 =''
+    text = self.request.get('text')
+    if text:
+      rot13 = text.encode('rot13')
 
-class Signup(webapp2.RequestHandler):
+    self.render('rot13.html', text = rot13)
+
+
+class Signup(BaseHandler):
   def get(self):
-    self.response.out.write(form3)
+    self.render('signup.html')
 
+  def post(self):
+    have_error = False
+    username = self.request.get('username')
+    password = self.request.get('password')
+    verify = self.request.get('verify')
+    email = self.request.get('email')
+
+    params = dict(username = username, email = email)
+
+    if not valid_username(username):
+      params['error_username'] = "That's not a valid username"
+      have_error = True
+
+    if not valid_password(password):
+      params['error_password'] = "That wasn't a valid password."
+      have_error = True
+    elif password != verify:
+      params['error_verify'] = "Your passwords didn't match."
+      have_error = True
+
+    if not valid_email(email):
+      params['error_email'] = "That's not a valid email."
+      have_error = True
+
+    if have_error:
+      self.render('signup.html', **params)
+    else:
+      self.redirect('/welcome?username=' + username)
+
+
+class Welcome(BaseHandler):
+  def get(self):
+    username = self.request.get('username')
+    if valid_username(username):
+      self.render('welcome.html', username = username)
+    else:
+      self.redirect('/signup')
 
 app = webapp2.WSGIApplication([('/', MainPage),
-  ('/rot13', Rot13), ('/signup', Signup)], debug=True)
+  ('/rot13', Rot13), ('/signup', Signup), ('/welcome', Welcome)], debug=True)
