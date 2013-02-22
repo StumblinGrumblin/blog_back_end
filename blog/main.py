1bba25ae70323ccd5085a1829027f2e874dc5a55
stumblingrumblin <stumblingrumblin@gmail.com>
2013-02-22 06:39:47 -0800

    Create Blog Structure

--- blog/main.py
+++ blog/main.py
@@ -20,20 +20,75 @@ def render_str(template, **params):
     return t.render(params)
 
 class BaseHandler(webapp2.RequestHandler):
-    def render(self, template, **kw):
-        self.response.out.write(render_str(template, **kw))
-
     def write(self, *a, **kw):
-        self.response.out.write(*a, **kw)
+      self.response.out.write(*a, **kw)
+
+    def render_str(self, template, **params):
+      return render_str(template, **params)
+
+    def render(self, template, **kw):
+        self.write(self.render_str(template, **kw))
+
+def render_post(response, post):
+  response.out.write('<b>' + post.subject + '</b><br>')
+  response.out.write(post.content)
+
 
 class MainPage(BaseHandler):
   def get(self):
     self.render('index.html')
 
+####BLOG HANDLERS####
+def blog_key(name = 'default'):
+    return db.Key.from_path('blogs', name)
+
+class Post(db.Model):
+    subject = db.StringProperty(required = True)
+    content = db.TextProperty(required = True)
+    created = db.DateTimeProperty(auto_now_add = True)
+    last_modified = db.DateTimeProperty(auto_now = True)
+
+    def render(self):
+        self._render_text = self.content.replace('\n', '<br>')
+        return render_str("/blog/post.html", p = self)
+
+class BlogFront(BaseHandler):
+  def get(self):
+    posts = db.GqlQuery("select * from Post order by created desc limit 10")
+    self.render('/blog/front.html', posts = posts)
+
+class PostPage(BaseHandler):
+  def get(self, post_id):
+    key = db.Key.from_path('Post', int(post_id), parent=blog_key())
+    post = db.get(key)
+
+    if not post:
+      self.error(404)
+      return
+
+    self.render("/blog/permalink.html", post = post)
+
+class NewPost(BaseHandler):
+  def get(self):
+    self.render("/blog/newpost.html")
+
+  def post(self):
+    subject = self.request.get('subject')
+    content = self.request.get('content')
+
+    if subject and content:
+      p = Post(parent = blog_key(), subject = subject, content = content)
+      p.put()
+      self.redirect('/blog/%s' % str(p.key().id()))
+    else:
+      error = "subject and content, please!"
+      self.render("/blog/newpost.html", subject=subject, content=content, error=error)
+
 class AboutPage(BaseHandler):
   def get(self):
     self.render('about.html')
 
+####OTHER PROJECTS####
 class Rot13(BaseHandler):
   
   def get(self):
@@ -89,4 +144,12 @@ class Welcome(BaseHandler):
     else:
       self.redirect('/signup')
 
-app = webapp2.WSGIApplication([('/', MainPage), ('/about', AboutPage), ('/rot13', Rot13), ('/signup', Signup), ('/welcome', Welcome)], debug=True)
+app = webapp2.WSGIApplication([('/', MainPage), ('/about', AboutPage), 
+                                ('/rot13', Rot13), 
+                                ('/signup', Signup), 
+                                ('/welcome', Welcome),
+                                ('/blog/?', BlogFront),
+                                ('/blog/([0-9]+)', PostPage),
+                                ('/blog/newpost', NewPost)
+                                ], 
+                                debug=True)
